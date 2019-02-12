15fdbb8f8423626d5161eb02c87eb3f3d6a7d461
CoderMe <srcii@coderme.com>
2019-02-12 19:45:24 +0200

    version 0.1a4
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/re.go
+++ src/re.go
@@ -15,6 +15,7 @@ var (
 		regexp.MustCompile(`(?is)(<[^<>]+)(\scrossorigin=["'][^"'<>]*["'])`),
 		// sintegrity="sha256-nas+AAA/ac129be5a99129c50ec2bf891b2b439ac02bedde2a73c6e91f683f79c43577b6"
 		regexp.MustCompile(`(?is)(<[^<>]+)(\sintegrity=["'][^"'<>]*["'])`),
+		regexp.MustCompile(`(?is)(<[^<>]+)(\stype=["']\s*text/javascript\s*["'])`),
 	}
 	reWhitespace = regexp.MustCompile(`(?s)\s+`)
 )
