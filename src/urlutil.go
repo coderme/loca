860cfeec358be4398c20eb8e89523d4bb208f520
CoderMe <srcii@coderme.com>
2019-02-24 20:44:15 +0200

    version 0.1a10
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/urlutil.go
+++ src/urlutil.go
@@ -8,17 +8,24 @@ var (
 	client = &http.Client{}
 )
 
-func buildRequest(u, ua string) *http.Request {
+func buildRequest(u, ua string) (*http.Request, error) {
 	req, err := http.NewRequest("GET", u, nil)
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Set("User-Agent", ua)
 
-	return ua, err
+	return req, err
 
 }
 
 func fetch(u string) (*http.Response, error) {
+	req, err := buildRequest(u, *userAgent)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return client.Do(req)
 
 }
