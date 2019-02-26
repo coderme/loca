8785c35bbae3c32476135573fd3e28732f85b3a6
CoderMe <srcii@coderme.com>
2019-02-26 12:37:45 +0200

    version 0.1a12
    Signed-off-by: CoderMe <srcii@coderme.com>

--- src/urlutil.go
+++ src/urlutil.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 var (
 	client = &http.Client{}
 )
 
+// buildRequest builds a HTTP request and sets a custom User Agent
 func buildRequest(u, ua string) (*http.Request, error) {
 	req, err := http.NewRequest("GET", u, nil)
 	if err != nil {
@@ -19,7 +22,11 @@ func buildRequest(u, ua string) (*http.Request, error) {
 
 }
 
-func fetch(u string) (*http.Response, error) {
+// fetch fetches a HTTP resource after the delay
+func fetch(u string, delay time.Duration) (*http.Response, error) {
+	// delayed fetching
+	<-time.After(delay)
+
 	req, err := buildRequest(u, *userAgent)
 
 	if err != nil {
@@ -27,5 +34,12 @@ func fetch(u string) (*http.Response, error) {
 	}
 
 	return client.Do(req)
+}
+
+// readFullResponse reads the full HTTP response as bytes, closes
+// HTTP response bdy after finish
+func readFullResponse(resp *http.Response) ([]byte, error) {
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
 
 }
